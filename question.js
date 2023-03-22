0dd80db7ac6f5a4751a7d2fb1bbd395ecadd34c1
harini1777 <126595486+harini1777@users.noreply.github.com>
2023-03-22 17:44:51 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,16 @@
+const responseObj = {
+    hi: "Hey ! How are you doing ?",
+    good : "How can i help you ?",
+    fine : "How can i help you ?",
+    Notbad : "How can i help you ?",
+    hey: "Hey! What's Up",
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+    "greeting": "Hello! Welcome to our college website. How can I help you today?",
+    "1": "For admissions information, please visit our admissions page or contact our admissions office at admissions@college.edu.",
+    "2": "We offer a variety of programs and courses in a wide range of fields. You can browse our offerings on our website or contact our academic advising office at advising@college.edu.",
+    "3": "We offer a range of financial aid and scholarship options for eligible students. You can learn more about these opportunities on our financial aid page or contact our financial aid office at finaid@college.edu.",
+    "4": "Our campus is home to a variety of clubs, organizations, and events. You can learn more about campus life and activities on our student life page or contact our student affairs office at studentaffairs@college.edu.",
+    "5": "We offer career services to help our students prepare for and find employment after graduation. You can learn more about these services on our career services page or contact our career services office at careerservices@college.edu.",
+    "6": "For all other inquiries, please contact our main office at info@college.edu and we will be happy to assist you."
+};
